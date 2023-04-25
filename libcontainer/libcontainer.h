32ae8a009e9c9d1af744dc8b3a3ea8c726e0c2ce
jramos <j.ramos@fanox.com>
2023-04-25 12:06:56 +0200

    Create LibContainer for container_of macro

---
+++ libcontainer/libcontainer.h
@@ -0,0 +1,3 @@
+#define libcontainer_of(ptr, type, member) ({                      				\
+						const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
+						(type *)( (char *)__mptr - offsetof(type,member) );})
