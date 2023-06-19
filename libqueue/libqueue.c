272825892b0a8a1b0cd5c3ed40bed3e8965bb8db
jramos <j.ramos@fanox.com>
2023-06-19 09:53:18 +0200

    Fix get_data functions name

--- libqueue/libqueue.c
+++ libqueue/libqueue.c
@@ -219,7 +219,7 @@ void * libqueue_get_data (Queue_t * queue)
 	return libqueue_get_data_first (queue);
 }
 
-void * libqueue_get_node_first (Queue_t * queue)
+void * libqueue_get_data_first (Queue_t * queue)
 {
 	switch (queue->type)
 	{
@@ -238,7 +238,7 @@ void * libqueue_get_node_first (Queue_t * queue)
     }
 }
 
-void * libqueue_get_node_last (Queue_t * queue)
+void * libqueue_get_data_last (Queue_t * queue)
 {
 	switch (queue->type)
 	{
@@ -257,12 +257,12 @@ void * libqueue_get_node_last (Queue_t * queue)
     }
 }
 
-void * libqueue_get_node_before (QNode_t * ref_node);
+void * libqueue_get_data_before (QNode_t * ref_node);
 {
 	return ref_node->before->data;
 }
 
-void * libqueue_get_node_after (QNode_t * ref_node);
+void * libqueue_get_data_after (QNode_t * ref_node);
 {
 	return ref_node->after->data;
 }
